bc4289f3266e8068df653330b6e6b2d37fd74bec
CoRex <dev@corex.dk>
2017-02-15 23:07:57 +0100

    Added unsetRequest() and developed unittest for System/Input.

---
+++ src/System/Input.php
@@ -0,0 +1,269 @@
+<?php
+
+namespace CoRex\Support\System;
+
+class Input
+{
+    /**
+     * Get url.
+     *
+     * @param boolean $addPath Default false.
+     * @return string
+     */
+    public static function getBaseUrl($addPath = false)
+    {
+        $url = 'http';
+        if (self::isSsl()) {
+            $url .= 's';
+        }
+        $url .= '://' . self::getHost();
+        if ($addPath) {
+            $path = self::getPath();
+            if ($path != '') {
+                $url .= '/' . $path;
+            }
+        }
+        return $url;
+    }
+
+    /**
+     * Get host.
+     *
+     * @return string
+     */
+    public static function getHost()
+    {
+        if (isset($_SERVER['HTTP_HOST'])) {
+            return $_SERVER['HTTP_HOST'];
+        }
+        return '';
+    }
+
+    /**
+     * Get domain.
+     *
+     * @return string
+     */
+    public static function getDomain()
+    {
+        return self::getHost();
+    }
+
+    /**
+     * Get method.
+     * @param boolean $strLower Default false.
+     *
+     * @return string
+     */
+    public static function getMethod($strLower = false)
+    {
+        $method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'GET';
+        if ($strLower) {
+            $method = strtolower($method);
+        } else {
+            $method = strtoupper($method);
+        }
+        return $method;
+    }
+
+    /**
+     * GEt protocol.
+     *
+     * @return string
+     */
+    public static function getProtocol()
+    {
+        $protocol = 'http';
+        if (self::isSsl()) {
+            $protocol .= 's';
+        }
+        return $protocol;
+    }
+
+    /**
+     * Is ssl.
+     *
+     * @return boolean
+     */
+    public static function isSsl()
+    {
+        $isSecure = false;
+        if (isset($_SERVER['HTTPS']) && in_array($_SERVER['HTTPS'], ['on', '1'])) {
+            $isSecure = true;
+        } elseif (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+            $isSecure = true;
+        } elseif (isset($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
+            $isSecure = true;
+        }
+        return $isSecure;
+    }
+
+    /**
+     * Get user-agent.
+     *
+     * @return string
+     */
+    public static function getUserAgent()
+    {
+        if (isset($_SERVER['HTTP_USER_AGENT'])) {
+            return $_SERVER['HTTP_USER_AGENT'];
+        }
+        return '';
+    }
+
+    /**
+     * Get remote-address.
+     *
+     * @return string
+     */
+    public static function getRemoteAddress()
+    {
+        if (isset($_SERVER['REMOTE_ADDR'])) {
+            return $_SERVER['REMOTE_ADDR'];
+        }
+        return '';
+    }
+
+    /**
+     * Get path.
+     *
+     * @return string
+     */
+    public static function getPath()
+    {
+        $uri = $_SERVER['REQUEST_URI'];
+        if (strpos($uri, '?') > 0) {
+            $uri = substr($uri, 0, strpos($uri, '?'));
+        }
+        $uri = preg_replace("/^\\/(.*)$/", "$1", $uri);
+        $uri = preg_replace("/^(.*)\\/$/", "$1", $uri);
+        return $uri;
+    }
+
+    /**
+     * Get query.
+     *
+     * @param string $name Default '' which means all.
+     * @param mixed $defaultValue Default null.
+     * @return array|mixed
+     */
+    public static function getQuery($name = '', $defaultValue = null)
+    {
+        parse_str($_SERVER['QUERY_STRING'], $queryStringParts);
+        if ($name != '') {
+            if (isset($queryStringParts[$name])) {
+                return $queryStringParts[$name];
+            } else {
+                return $defaultValue;
+            }
+        }
+        return $queryStringParts;
+    }
+
+    /**
+     * Get request.
+     *
+     * @param string $name
+     * @param mixed $defaultValue Default null.
+     * @return mixed
+     */
+    public static function getRequest($name, $defaultValue = null)
+    {
+        if (isset($_REQUEST[$name])) {
+            return $_REQUEST[$name];
+        }
+        return $defaultValue;
+    }
+
+    /**
+     * Set request.
+     *
+     * @param string $name
+     * @param mixed $value
+     */
+    public static function setRequest($name, $value)
+    {
+        $_REQUEST[$name] = $value;
+    }
+
+    /**
+     * Unset request.
+     *
+     * @param string $name
+     */
+    public static function unsetRequest($name)
+    {
+        if (self::requestExist($name)) {
+            unset($_REQUEST[$name]);
+        }
+    }
+
+    /**
+     * Request exist.
+     *
+     * @param string $name
+     * @return boolean
+     */
+    public static function requestExist($name)
+    {
+        return isset($_REQUEST[$name]);
+    }
+
+    /**
+     * Get headers.
+     *
+     * @return array|false
+     */
+    public static function getHeaders()
+    {
+        return getallheaders();
+    }
+
+    /**
+     * Get header.
+     *
+     * @param string $header
+     * @param string $defaultValue
+     * @return mixed|string
+     */
+    public static function getHeader($header, $defaultValue = '')
+    {
+        $headers = self::getHeaders();
+        if (is_array($headers) && isset($headers[$header])) {
+            return $headers[$header];
+        }
+        return $defaultValue;
+    }
+
+    /**
+     * Get header Content-Type.
+     *
+     * @param string $defaultValue Default ''.
+     * @return mixed|string
+     */
+    public static function getHeaderContentType($defaultValue = '')
+    {
+        return self::getHeader('Content-Type', $defaultValue);
+    }
+
+    /**
+     * Get header Accept.
+     *
+     * @param string $defaultValue Default ''.
+     * @return mixed|string
+     */
+    public static function getHeaderAccept($defaultValue = '')
+    {
+        return self::getHeader('Accept', $defaultValue);
+    }
+
+    /**
+     * Return body.
+     *
+     * @return string
+     */
+    public static function getBody()
+    {
+        return file_get_contents('php://input');
+    }
+}
---
+++ tests/System/InputTest.php
@@ -0,0 +1,270 @@
+<?php
+
+use CoRex\Support\System\Input;
+
+class InputTest extends PHPUnit_Framework_TestCase
+{
+    const HTTP_HOST = 'test.host';
+    const REQUEST_METHOD = 'PaTcH';
+    const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36';
+    const IP = '1.2.3.4';
+    const URI = '/test1/test2?firstname=Roger&lastname=Moore&encoded=%28%C3%A6%C3%B8%C3%A5%C3%86%C3%98%C3%85%29';
+    const QUERY_STRING = 'firstname=Roger&lastname=Moore&encoded=%28%C3%A6%C3%B8%C3%A5%C3%86%C3%98%C3%85%29';
+    const HEADER_CONTENT_TYPE = 'application/type';
+    const HEADER_ACCEPT = 'application/type';
+    const TEST = 'test';
+    const PATH = 'test1/test2';
+    const QUERY_ARRAY = [
+        'firstname' => 'Roger',
+        'lastname' => 'Moore',
+        'encoded' => '(æøåÆØÅ)'
+    ];
+
+    /**
+     * Setup.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        // Setup basic server variables.
+        $_SERVER['HTTP_HOST'] = self::HTTP_HOST;
+        $_SERVER['REQUEST_METHOD'] = self::REQUEST_METHOD;
+        $_SERVER['HTTP_USER_AGENT'] = self::USER_AGENT;
+        $_SERVER['REMOTE_ADDR'] = self::IP;
+        $_SERVER['REQUEST_URI'] = self::URI;
+        $_SERVER['QUERY_STRING'] = self::QUERY_STRING;
+
+        // Make sure function getallheaders() exists for tests.
+        if (!function_exists('getallheaders')) {
+            function getallheaders()
+            {
+                return [
+                    self::TEST => self::TEST,
+                    'Content-Type' => self::HEADER_CONTENT_TYPE,
+                    'Accept' => self::HEADER_ACCEPT
+                ];
+            }
+        }
+    }
+
+    /**
+     * Test get base url.
+     */
+    public function testGetBaseUrl()
+    {
+        $this->setSslEntries(0);
+        $this->assertEquals('http://' . self::HTTP_HOST, Input::getBaseUrl());
+        $this->assertEquals('http://' . self::HTTP_HOST . '/' . self::PATH, Input::getBaseUrl(true));
+        $this->setSslEntries(1);
+        $this->assertEquals('https://' . self::HTTP_HOST, Input::getBaseUrl());
+        $this->setSslEntries(2);
+        $this->assertEquals('https://' . self::HTTP_HOST, Input::getBaseUrl());
+        $this->setSslEntries(3);
+        $this->assertEquals('https://' . self::HTTP_HOST, Input::getBaseUrl());
+        $this->setSslEntries(4);
+        $this->assertEquals('https://' . self::HTTP_HOST, Input::getBaseUrl());
+    }
+
+    /**
+     * Test get host.
+     */
+    public function testGetHost()
+    {
+        $this->assertEquals(self::HTTP_HOST, Input::getHost());
+    }
+
+    /**
+     * Test get domain.
+     */
+    public function testGetDomain()
+    {
+        $this->assertEquals(self::HTTP_HOST, Input::getDomain());
+    }
+
+    /**
+     * Test get method.
+     */
+    public function testGetMethod()
+    {
+        $this->assertEquals(strtoupper(self::REQUEST_METHOD), Input::getMethod());
+        $this->assertEquals(strtolower(self::REQUEST_METHOD), Input::getMethod(true));
+    }
+
+    /**
+     * Test get protocol.
+     */
+    public function testGetProtocol()
+    {
+        $this->setSslEntries(0);
+        $this->assertEquals('http', Input::getProtocol());
+        $this->setSslEntries(1);
+        $this->assertEquals('https', Input::getProtocol());
+        $this->setSslEntries(2);
+        $this->assertEquals('https', Input::getProtocol());
+        $this->setSslEntries(3);
+        $this->assertEquals('https', Input::getProtocol());
+        $this->setSslEntries(4);
+        $this->assertEquals('https', Input::getProtocol());
+    }
+
+    /**
+     * Test is ssl.
+     */
+    public function testIsSsl()
+    {
+        $this->setSslEntries(0);
+        $this->assertFalse(Input::isSsl());
+        $this->setSslEntries(1);
+        $this->assertTrue(Input::isSsl());
+        $this->setSslEntries(2);
+        $this->assertTrue(Input::isSsl());
+        $this->setSslEntries(3);
+        $this->assertTrue(Input::isSsl());
+        $this->setSslEntries(4);
+        $this->assertTrue(Input::isSsl());
+    }
+
+    /**
+     * Test get user agent.
+     */
+    public function testGetUserAgent()
+    {
+        $this->assertEquals(self::USER_AGENT, Input::getUserAgent());
+    }
+
+    /**
+     * Test get remote address.
+     */
+    public function testGetRemoteAddress()
+    {
+        $this->assertEquals(self::IP, Input::getRemoteAddress());
+    }
+
+    /**
+     * Test get path.
+     */
+    public function testGetPath()
+    {
+        $this->assertEquals(self::PATH, Input::getPath());
+    }
+
+    /**
+     * Test get query.
+     */
+    public function testGetQuery()
+    {
+        $this->assertEquals(self::QUERY_ARRAY, Input::getQuery());
+        $this->assertEquals(self::QUERY_ARRAY['encoded'], Input::getQuery('encoded'));
+        $this->assertNull(Input::getQuery('unknown'));
+    }
+
+    /**
+     * Test get request.
+     */
+    public function testGetRequest()
+    {
+        Input::unsetRequest(self::TEST);
+        $this->assertNull(Input::getRequest(self::TEST));
+        Input::setRequest(self::TEST, self::TEST);
+        $this->assertEquals(self::TEST, Input::getRequest(self::TEST));
+        $this->assertNull(Input::getRequest('unknown'));
+    }
+
+    /**
+     * Test set request.
+     */
+    public function testSetRequest()
+    {
+        $this->testGetRequest();
+    }
+
+    /**
+     * Test unset request.
+     */
+    public function testUnsetRequest()
+    {
+        $this->testGetRequest();
+    }
+
+    /**
+     * Test request exist.
+     */
+    public function testRequestExist()
+    {
+        Input::unsetRequest(self::TEST);
+        $this->assertFalse(Input::requestExist(self::TEST));
+        Input::setRequest(self::TEST, self::TEST);
+        $this->assertTrue(Input::requestExist(self::TEST));
+    }
+
+    /**
+     * Test get headers.
+     */
+    public function testGetHeaders()
+    {
+        $headers = Input::getHeaders();
+        $this->assertEquals(self::TEST, $headers[self::TEST]);
+        $this->assertEquals(self::HEADER_CONTENT_TYPE, $headers['Content-Type']);
+        $this->assertEquals(self::HEADER_ACCEPT, $headers['Accept']);
+    }
+
+    /**
+     * Test get header.
+     */
+    public function testGetHeader()
+    {
+        $this->assertEquals(self::TEST, Input::getHeader(self::TEST));
+    }
+
+    /**
+     * Test get header Content-Type.
+     */
+    public function testGetHeaderContentType()
+    {
+        $this->assertEquals(self::HEADER_CONTENT_TYPE, Input::getHeaderContentType());
+    }
+
+    /**
+     * Test get header Accept.
+     */
+    public function testGetHeaderAccept()
+    {
+        $this->assertEquals(self::HEADER_ACCEPT, Input::getHeaderAccept());
+    }
+
+    /**
+     * Test get body.
+     */
+    public function testGetBody()
+    {
+        // Not testable.
+    }
+
+    /**
+     * Set ssl entries.
+     *
+     * @param integer $entryNumber
+     */
+    private function setSslEntries($entryNumber)
+    {
+        if (isset($_SERVER['HTTPS'])) {
+            unset($_SERVER['HTTPS']);
+        }
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
+            unset($_SERVER['HTTP_X_FORWARDED_PROTO']);
+        }
+        if (isset($_SERVER['HTTP_X_FORWARDED_SSL'])) {
+            unset($_SERVER['HTTP_X_FORWARDED_SSL']);
+        }
+        if ($entryNumber == 1) {
+            $_SERVER['HTTPS'] = 'on';
+        } elseif ($entryNumber == 2) {
+            $_SERVER['HTTPS'] = '1';
+        } elseif ($entryNumber == 3) {
+            $_SERVER['HTTP_X_FORWARDED_PROTO'] = 'https';
+        } elseif ($entryNumber == 4) {
+            $_SERVER['HTTP_X_FORWARDED_SSL'] = 'on';
+        }
+    }
+}
