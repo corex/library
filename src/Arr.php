60da69954abb03c75471cddb287715c703ea865b
CoRex <dev@corex.dk>
2017-02-16 15:52:30 +0100

    Updated Arr class and wrote tests.

--- src/Arr.php
+++ src/Arr.php
@@ -8,16 +8,66 @@ class Arr
      * Get value from data.
      *
      * @param array $data
-     * @param string $key
+     * @param string $path Uses dot notation.
      * @param mixed $defaultValue Default null.
      * @return mixed|null
      */
-    public static function get($data, $key, $defaultValue = null)
+    public static function get(array $data, $path, $defaultValue = null)
     {
-        if (is_array($data) && isset($data[$key])) {
-            return $data[$key];
+        $data = self::getDataByPath($data, $path);
+        if ($data === null) {
+            $data = $defaultValue;
         }
-        return $defaultValue;
+        return $data;
+    }
+
+    /**
+     * Set value in data.
+     *
+     * @param array $array
+     * @param string $path Uses dot notation.
+     * @param mixed $value
+     * @param boolean $create Default false.
+     * @return array|mixed
+     */
+    public static function set(array &$array, $path, $value, $create = false)
+    {
+        if (!is_array($array)) {
+            return $array;
+        }
+
+        // Extract key/path.
+        $pathKey = Str::getLast($path, '.');
+        $path = Str::removeLast($path, '.');
+
+        // Extract data.
+        $pathArray = null;
+        if ($path != '' && $path !== null) {
+            $array = &self::getDataByPath($array, $path, $create);
+        }
+        if ($array !== null || $create) {
+            $array[$pathKey] = $value;
+        }
+    }
+
+    /**
+     * Get first element of array.
+     *
+     * @param array $data
+     * @param string $key Get key from element if array. Default null.
+     * @return mixed
+     */
+    public static function getFirst(array $data, $key = null)
+    {
+        if (count($data) == 0) {
+            return null;
+        }
+        reset($data);
+        $element = current($data);
+        if ($key !== null && is_array($element) && isset($element[$key])) {
+            return $element[$key];
+        }
+        return $element;
     }
 
     /**
@@ -73,20 +123,212 @@ class Arr
     }
 
     /**
-     * To associative.
+     * Is string in list.
      *
-     * @param array $values
-     * @param $key
+     * @param array $list
+     * @param string $key Default null.
+     * @return boolean
+     */
+    public static function isStringInList(array $list, $key = null)
+    {
+        $stringInList = false;
+        if (count($list) == 0) {
+            return $stringInList;
+        }
+        foreach ($list as $item) {
+            if ($key !== null && isset($item[$key])) {
+                $value = $item[$key];
+            } else {
+                $value = $item;
+            }
+            if (!is_numeric($value)) {
+                $stringInList = true;
+            }
+        }
+        return $stringInList;
+    }
+
+    /**
+     * Index of.
+     * Note: Supports array with objects.
+     *
+     * @param array $array
+     * @param string $value
+     * @param string $key Default null which means the item itself (not associative array).
+     * @return integer -1 if not found.
+     */
+    public static function indexOf(array $array, $value, $key = null)
+    {
+        if (!is_array($array)) {
+            return -1;
+        }
+        foreach ($array as $index => $item) {
+            if ($key !== null) {
+                if (is_object($item)) {
+                    $checkValue = isset($item->{$key}) ? $item->{$key} : null;
+                } else {
+                    $checkValue = isset($item[$key]) ? $item[$key] : null;
+                }
+            } else {
+                $checkValue = $item;
+            }
+            if ($checkValue !== null && $checkValue === $value) {
+                return $index;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Check if all keys exist in array.
+     *
+     * @param array $data
+     * @param array $keys
+     * @return boolean
+     */
+    public static function keysExist(array $data, array $keys)
+    {
+        if (count($keys) > 0) {
+            foreach ($keys as $key) {
+                if (!array_key_exists($key, $data)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Keys.
+     *
+     * @param array $array
      * @return array
      */
-    public static function toAssociative(array $values, $key)
+    public static function keys(array $array)
+    {
+        return array_keys($array);
+    }
+
+    /**
+     * Values (0..n).
+     *
+     * @param array $array
+     * @return array
+     */
+    public static function values(array $array)
+    {
+        return array_values($array);
+    }
+
+    /**
+     * Is associative.
+     *
+     * @param array $array
+     * @return boolean
+     */
+    public static function isAssociative(array $array)
+    {
+        $keys = array_keys($array);
+        return array_keys($keys) !== $keys;
+    }
+
+    /**
+     * Pluck.
+     *
+     * @param array $array
+     * @param string $path Uses dot notation.
+     * @param mixed $defaultValue Default null.
+     * @return array
+     */
+    public static function pluck(array $array, $path, $defaultValue = null)
     {
         $result = [];
-        if (count($values) > 0) {
-            foreach ($values as $value) {
-                $result[] = [$key => $value];
+        if (!is_array($array)) {
+            return $result;
+        }
+
+        // Extract key/path.
+        $pathKey = Str::getLast($path, '.');
+        $path = Str::removeLast($path, '.');
+
+        // Extract data.
+        if ($path != '' && $path !== null) {
+            $array = self::getDataByPath($array, $path);
+        }
+        foreach ($array as $item) {
+            $value = $defaultValue;
+            if (is_object($item) && isset($item->{$pathKey})) {
+                $value = $item->{$pathKey};
+            } elseif (is_array($item) && isset($item[$pathKey])) {
+                $value = $item[$pathKey];
+            }
+            $result[] = $value;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get line match.
+     *
+     * @param array $lines
+     * @param string $prefix
+     * @param string $suffix
+     * @param boolean $doTrim
+     * @param boolean $removePrefixSuffix Default false.
+     * @return array
+     */
+    public static function getLineMatch(array $lines, $prefix, $suffix, $doTrim, $removePrefixSuffix = false)
+    {
+        $result = [];
+        foreach ($lines as $line) {
+            $isHit = true;
+            if ($prefix != '' && $prefix !== null && Str::startsWith(trim($line), $prefix)) {
+                if ($removePrefixSuffix) {
+                    $line = substr(trim($line), strlen($prefix));
+                }
+            } else {
+                $isHit = false;
+            }
+            if ($suffix != '' && $suffix !== null && Str::endsWith(trim($line), $suffix)) {
+                if ($removePrefixSuffix) {
+                    $line = substr(trim($line), 0, -strlen($suffix));
+                }
+            } else {
+                $isHit = false;
+            }
+            if ($isHit) {
+                if ($doTrim) {
+                    $line = trim($line);
+                }
+                $result[] = $line;
             }
         }
         return $result;
+    }
+
+    /**
+     * Get data by path.
+     *
+     * @param array $data
+     * @param string $path
+     * @param boolean $create Default false.
+     * @param mixed $defaultValue Default null.
+     * @return mixed
+     * @throws \Exception
+     */
+    private static function &getDataByPath(array &$data, $path, $create = false, $defaultValue = null)
+    {
+        if ((string)$path == '') {
+            return $data;
+        }
+        $pathSegments = explode('.', $path);
+        foreach ($pathSegments as $pathSegment) {
+            if (!isset($data[$pathSegment]) && !$create) {
+                return $defaultValue;
+            }
+            $data = &$data[$pathSegment];
+        }
+        return $data;
     }
 }
---
+++ tests/ArrTest.php
@@ -0,0 +1,187 @@
+<?php
+
+use CoRex\Support\Arr;
+
+class ArrTest extends PHPUnit_Framework_TestCase
+{
+    private $actor1 = ['firstname' => 'Sean', 'lastname' => 'Connery'];
+    private $actor2 = ['firstname' => 'Roger', 'lastname' => 'Moore'];
+    private $actor3 = ['firstname' => 'Timothy', 'lastname' => 'Dalton'];
+    private $actor4 = ['firstname' => 'Pierce', 'lastname' => 'Brosnan'];
+    private $actor5 = ['firstname' => 'Daniel', 'lastname' => 'Craig'];
+
+    /**
+     * Test get.
+     */
+    public function testGet()
+    {
+        $data = ['actor' => $this->actor1];
+        $this->assertEquals($this->actor1['firstname'], Arr::get($data, 'actor.firstname'));
+        $this->assertEquals($this->actor1['lastname'], Arr::get($data, 'actor.lastname'));
+        $this->assertEquals('test', Arr::get($data, 'actor.test', 'test'));
+        $this->assertNull(Arr::get($data, 'actor.test'));
+    }
+
+    /**
+     * Test set.
+     */
+    public function testSet()
+    {
+        $data = [];
+        $this->assertNull(Arr::get($data, 'actor.test1.test2'));
+        Arr::set($data, 'actor.test1.test2', 'testing', true);
+        $this->assertEquals('testing', Arr::get($data, 'actor.test1.test2'));
+    }
+
+    /**
+     * Test get first.
+     */
+    public function testGetFirst()
+    {
+        $data = [$this->actor1, $this->actor2, $this->actor3, $this->actor4, $this->actor5];
+        $this->assertEquals($this->actor1, Arr::getFirst($data));
+        $this->assertEquals($this->actor1['firstname'], Arr::getFirst($data, 'firstname'));
+    }
+
+    /**
+     * Test get last.
+     */
+    public function testGetLast()
+    {
+        $data = [$this->actor1, $this->actor2, $this->actor3, $this->actor4, $this->actor5];
+        $this->assertEquals($this->actor5, Arr::getLast($data));
+        $this->assertEquals($this->actor5['firstname'], Arr::getLast($data, 'firstname'));
+    }
+
+    /**
+     * Test remove last.
+     */
+    public function testRemoveLast()
+    {
+        $data = [$this->actor1, $this->actor2, $this->actor3, $this->actor4, $this->actor5];
+        $data = Arr::removeLast($data);
+        $this->assertEquals($this->actor4, Arr::getLast($data));
+    }
+
+    /**
+     * Test is list.
+     */
+    public function testIsList()
+    {
+        $data = [
+            'actor1' => $this->actor1,
+            'actor2' => $this->actor2,
+            'actor3' => $this->actor3,
+            'actor4' => $this->actor4,
+            'actor5' => $this->actor5
+        ];
+        $this->assertFalse(Arr::isList($data));
+        $data = [$this->actor1, $this->actor2, $this->actor3, $this->actor4, $this->actor5];
+        $this->assertTrue(Arr::isList($data));
+    }
+
+    /**
+     * Test is string in list.
+     */
+    public function testIsStringInList()
+    {
+        $data = [4345, 435, 234, 43, 435, 345, 2354];
+        $this->assertFalse(Arr::isStringInList($data));
+        $data = [$this->actor1, $this->actor2, $this->actor3, $this->actor4, $this->actor5];
+        $this->assertTrue(Arr::isStringInList($data, 'firstname'));
+    }
+
+    /**
+     * Test index of.
+     */
+    public function testIndexOf()
+    {
+        // Test simple array.
+        $data = ['test1', 'test2', 'test3'];
+        $this->assertEquals(1, Arr::indexOf($data, 'test2'));
+
+        // Test simple array with associative item.
+        $data = [$this->actor1, $this->actor2, $this->actor3, $this->actor4, $this->actor5];
+        $this->assertEquals(1, Arr::indexOf($data, $this->actor2['firstname'], 'firstname'));
+
+        // Test aasociative array.
+        $data = ['test1' => 'test1', 'test2' => 'test2', 'test3' => 'test3'];
+        $this->assertEquals('test2', Arr::indexOf($data, 'test2'));
+
+        // Test aasociative array with associative item.
+        $data = [
+            'actor1' => $this->actor1,
+            'actor2' => $this->actor2,
+            'actor3' => $this->actor3,
+            'actor4' => $this->actor4,
+            'actor5' => $this->actor5
+        ];
+        $this->assertEquals('actor2', Arr::indexOf($data, $this->actor2['firstname'], 'firstname'));
+    }
+
+    /**
+     * Test keys exist.
+     */
+    public function testKeysExist()
+    {
+        $data = ['actor1' => 'test', 'actor2' => 'test', 'actor3' => 'test', 'actor4' => 'test'];
+        $this->assertFalse(Arr::keysExist($data, ['unknown', 'actor3']));
+        $this->assertTrue(Arr::keysExist($data, ['actor1', 'actor3']));
+    }
+
+    /**
+     * Test keys.
+     */
+    public function testKeys()
+    {
+        $data = ['actor1' => 'test', 'actor2' => 'test', 'actor3' => 'test', 'actor4' => 'test'];
+        $this->assertEquals(array_keys($data), Arr::keys($data));
+    }
+
+    /**
+     * Test is associative.
+     */
+    public function testIsAssociative()
+    {
+        $data = ['test1', 'test2', 'test3'];
+        $this->assertFalse(Arr::isAssociative($data));
+        $data = ['actor1' => 'test', 'actor2' => 'test', 'actor3' => 'test', 'actor4' => 'test'];
+        $this->assertTrue(Arr::isAssociative($data));
+    }
+
+    /**
+     * Test pluck.
+     */
+    public function testPluck()
+    {
+        $checkData = [$this->actor1['firstname'], $this->actor2['firstname']];
+
+        // Test simple array with associative item.
+        $data = [$this->actor1, $this->actor2];
+        $this->assertEquals($checkData, Arr::pluck($data, 'firstname'));
+
+        // Test aasociative array.
+        $data = ['test1' => 'test1', 'test2' => 'test2', 'test3' => 'test3'];
+        $this->assertEquals([null, null, null], Arr::pluck($data, 'test2'));
+    }
+
+    /**
+     * Test get line match.
+     */
+    public function testGetLineMatch()
+    {
+        $lines = [
+            '         use CoRex\Database\Command\CommandBase;                   ',
+            '                          use CoRex\Database\Interfaces\ConnectorInterface;                ',
+            'use CoRex\Support\System\Directory;               ',
+            '            use CoRex\Support\System\Template;       '
+        ];
+        $linesMatch = [
+            'CoRex\Database\Command\CommandBase',
+            'CoRex\Database\Interfaces\ConnectorInterface',
+            'CoRex\Support\System\Directory',
+            'CoRex\Support\System\Template'
+        ];
+        $this->assertEquals($linesMatch, Arr::getLineMatch($lines, 'use ', ';', true, true));
+    }
+}
